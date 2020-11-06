e90bf755dfc8a763158d6658df09011535c3c931
kyuBin422 <yubinuestc@gmail.com>
2020-11-06 10:18:51 +0800

    Add files via upload

--- measurePDF.m
+++ measurePDF.m
@@ -3,34 +3,41 @@ clc;close all
 
 % % marginal probility density funcion
 % % optima q distrbution
-% generatePDF(qList.');
+% [TrainMetric,TestMetric]=generatePDF(qList.');
 % title('q* probability density function','Interpreter','latex')
 % xlabel('$q$','Interpreter','latex');
 %
 % % optima hat_theta_q distrbution
-% generatePDF(thetaqList.');
-% title('$\hat{\theta}_q$ probability density function','Interpreter','latex')
-% xlabel('$\hat{\theta}_q$','Interpreter','latex');
+[TrainMetric,TestMetric]=generatePDF(thetaqList.')
+title('$\hat{\theta}_q$ probability density function','Interpreter','latex')
+xlabel('$\hat{\theta}_q$','Interpreter','latex');
 %
 % % joint distrbution
 % % jointDistrubution(thetaqList.',qList.')
 
-validationMLE(qList.')
 
 
 
 
 
-function [Wei,Gamma,Lognormal,Normal]=generatePDF(data)
+
+function [TrainMetric,TestMetric]=generatePDF(data)
+%split the set as train set and test set
+
+flag=randsample(length(data),fix(length(data)*0.8));
+trainX=data(flag);
+data(flag)=[];
+testX=data;
 % fit the  distribution
-Wei=fitdist(data,'Weibull');
-Gamma=fitdist(data,'Gamma');
-Lognormal=fitdist(data,'Lognormal');
-Normal=fitdist(data,'Normal');
-
+Wei=fitdist(trainX,'Weibull');
+Gamma=fitdist(trainX,'Gamma');
+Lognormal=fitdist(trainX,'Lognormal');
+Normal=fitdist(trainX,'Normal');
+% output the negativate log likelihood
+TrainMetric=struct('Weibull',Wei.negloglik/length(trainX),'Gamma',Gamma.negloglik/length(trainX),...
+    'Lognormal',Lognormal.negloglik/length(trainX),'Normal',Normal.negloglik/length(trainX));
 % generate the pdf
-x=linspace(0,max(data),length(data));
-
+x=linspace(0,max(trainX),length(trainX));
 
 pdf_Wei = pdf(Wei,x);
 pdf_Gamma = pdf(Gamma,x);
@@ -43,7 +50,7 @@ pdf_Normal=pdf(Normal,x);
 % [~,Normal_p] = kstest(data,'CDF',y);
 
 figure
-histogram(data,20,'Normalization','pdf')
+histogram(trainX,20,'Normalization','pdf')
 line(x,pdf_Wei,'LineStyle','-','Color','r')
 line(x,pdf_Gamma,'LineStyle','-.','Color','b')
 line(x,pdf_Lognormal,'LineStyle','--','Color','g')
@@ -54,9 +61,22 @@ legend('Data','Weibull','Gamma','Lognormal','Normal','Location','northwest')
 
 
 ylabel('probability')
+% generate the test pdf
+
+pdf_Wei = pdf(Wei,testX);
+pdf_Gamma = pdf(Gamma,testX);
+pdf_Lognormal = pdf(Lognormal,testX);
+pdf_Normal=pdf(Normal,testX);
+TestMetric=struct(...
+    'Weibull',-sum(log(pdf_Wei))/length(testX),...
+    'Gamma',-sum(log(pdf_Gamma))/length(testX),...
+    'Lognormal',-sum(log(pdf_Lognormal))/length(testX),...
+    'Normal',-sum(log(pdf_Normal))/length(testX)...
+    );
 end
 
 function validationMLE(data)
+% divergence
 flag=randsample(length(data),fix(length(data)*0.8));
 trainX=data(flag);
 data(flag)=[];
