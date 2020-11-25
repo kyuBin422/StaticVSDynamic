542b84034fdef178a06eb78b8a8abb3068c3af2d
kyuBin422 <yubinuestc@gmail.com>
2020-11-25 18:43:13 +0800

    Delete dynamicStatistics.m

--- dynamicStatistics.m
+++
@@ -1,9 +0,0 @@
-clear;clc;close all
-addpath(genpath('utils'));
-format long
-load matlab_x0=1point3.mat;
-
-% calculate the expected omega and q
-disp(mean(omegaStar(thetaqList,qList)))
-
-disp(mean(qList))
