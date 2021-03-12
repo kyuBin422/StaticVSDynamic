878a11d6178915a0a78a25136ff080c9f4c88146
kyuBin422 <yubinuestc@gmail.com>
2021-03-12 16:49:30 +0800

    Delete normalizingConstant.m

--- utils/normalizingConstant.m
+++
@@ -1,6 +0,0 @@
-function a=normalizingConstant(chi,psi,lambda,sigma,gamma)
-% ref https://projecteuclid.org/download/pdfview_1/euclid.jam/1425305853
-% eqution 17
-a=(chi.*psi).^(-lambda/2).*psi.^lambda.*(psi+(gamma.^2./sigma.^2)).^(1/2-lambda)./((2*pi)^0.5...
-    .*sigma.*besselk(lambda,sqrt(psi.*chi)));
-end
